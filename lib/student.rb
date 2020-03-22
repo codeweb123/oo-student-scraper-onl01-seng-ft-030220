de9b6a0a2081dce2dfc1b7283b19922c5dc5f7c8
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 22:03:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -21,4 +21,3 @@ class Student
     @@all
   end
 end
-
